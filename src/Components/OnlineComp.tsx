c0a77fc5094f12433aa214f742587c897b1aa104
dazzlerabhi30800 <abhisingh30800@gmail.com>
2024-01-09 20:54:14 +0530

    added scroll animation in post section

--- src/Components/OnlineComp.tsx
+++ src/Components/OnlineComp.tsx
@@ -1,16 +1,34 @@
+import { useEffect } from "react";
+import { gsap } from "gsap";
 
-export default function OnlineComp({item}: {item: postType}) {
+export default function OnlineComp({ item }: { item: postType }) {
+  useEffect(() => {
+    document.querySelectorAll(".post").forEach((container) => {
+      gsap.fromTo(
+        container,
+        { scaleX: 0, opacity: 0, duration: 1 },
+        {
+          scaleX: 1,
+          opacity: 1,
+          duration: 1,
+          ease: "elastic.out(i, 1)",
+          scrollTrigger: {
+            trigger: container,
+            start: "0% 83%",
+            end: "bottom 100%",
+          },
+        },
+      );
+    });
+  }, []);
   return (
     <div className="post--container">
       <img src={item.img} alt={item.title} />
       <div className="post">
         <h2>{item.title}</h2>
         {item.description.map((desc: string, index: number) => (
-          <p key={index}>
-            {desc}
-          </p>
+          <p key={index}>{desc}</p>
         ))}
-        
       </div>
     </div>
   );
